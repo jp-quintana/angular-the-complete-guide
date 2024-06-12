5b59a98d0d9ad5228d15f4efdd297ad81d9c44a8
jp-quintana <juanquintana1996@gmail.com>
2024-06-12 00:42:13 -0300

    add first angular app

--- first-angular-app/src/app/tasks/tasks.service.ts
+++ first-angular-app/src/app/tasks/tasks.service.ts
@@ -6,6 +6,14 @@ import { NewTask } from './tasks.model';
 export class TasksService {
   private tasks = DUMMY_TASKS;
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -18,10 +26,15 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.date,
     });
-    console.log(this.tasks);
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
