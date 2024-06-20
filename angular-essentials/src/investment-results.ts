259d4643c5636687b96ad2d5fb554bf3980bef79
jp-quintana <juanquintana1996@gmail.com>
2024-06-20 19:10:09 -0300

    wip3: angular essentials

--- angular-essentials/src/investment-results.ts
+++ angular-essentials/src/investment-results.ts
@@ -2,7 +2,17 @@
 // e.g., integrate it into a service or component
 // You may need to tweak it, depending on where and how you use it
 
-function calculateInvestmentResults() {
+function calculateInvestmentResults({
+  initialInvestment,
+  annualInvestment,
+  expectedReturn,
+  duration,
+}: {
+  initialInvestment: number;
+  annualInvestment: number;
+  expectedReturn: number;
+  duration: number;
+}) {
   const annualData = [];
   let investmentValue = initialInvestment;
 
