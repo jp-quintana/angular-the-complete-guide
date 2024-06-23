8e106d6d511ff1ba151370219b49a9e83eaf599c
jp-quintana <juanquintana1996@gmail.com>
2024-06-23 18:51:16 -0300

    wip3: components and templates

--- components-and-templates/src/app/shared/control/control.component.ts
+++ components-and-templates/src/app/shared/control/control.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  inject,
+  Input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -9,8 +16,20 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
   host: {
     class: 'control',
+    '(click)': 'onClick()',
   },
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control'; <-- discouraged
+  // @HostListener('click') onClick() {
+  //   console.log('clicked')
+  // } <-- discouraged
   @Input({ required: true }) label!: string;
+
+  private el = inject(ElementRef);
+
+  onClick() {
+    console.log('clicked');
+    console.log(this.el.nativeElement);
+  }
 }
