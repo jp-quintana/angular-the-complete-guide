04563b4a34c62807f5faa55646575fc8a6883fe2
jp-quintana <juanquintana1996@gmail.com>
2024-07-11 17:00:52 -0300

    wip2: http requests responses

--- http-requests-responses/src/app/places/places.service.ts
+++ http-requests-responses/src/app/places/places.service.ts
@@ -1,20 +1,75 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 
 import { Place } from './place.model';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlacesService {
-  private userPlaces = signal<Place[]>([]);
+  httpClient = inject(HttpClient);
 
+  private userPlaces = signal<Place[]>([]);
   loadedUserPlaces = this.userPlaces.asReadonly();
 
-  loadAvailablePlaces() {}
+  loadAvailablePlaces() {
+    return this.fetchPlaces(
+      'http://localhost:3000/places',
+      'Something went wrong! Please try again later!'
+    );
+  }
 
-  loadUserPlaces() {}
+  loadUserPlaces() {
+    return this.fetchPlaces(
+      'http://localhost:3000/user-places',
+      'Something went wrong! Please try again later!'
+    ).pipe(
+      tap({
+        next: (userPlaces) => {
+          this.userPlaces.set(userPlaces);
+        },
+      })
+    );
+  }
 
-  addPlaceToUserPlaces(place: Place) {}
+  addPlaceToUserPlaces(place: Place) {
+    const prevPlaces = this.userPlaces();
+    if (!prevPlaces.some((p) => p.id === place.id)) {
+      this.userPlaces.set([...prevPlaces, place]);
+    }
+
+    return this.httpClient
+      .put('http://localhost:3000/user-places', {
+        placeId: place.id,
+      })
+      .pipe(
+        catchError((error) => {
+          console.log(error);
+          this.userPlaces.set(prevPlaces);
+          return throwError(() => new Error('Failed to store selected place.'));
+        })
+      );
+  }
 
   removeUserPlace(place: Place) {}
+
+  private fetchPlaces(url: string, errorMessage: string) {
+    return (
+      this.httpClient
+        .get<{ places: Place[] }>(url)
+        // second argument => { observe: 'response' }
+        .pipe(
+          map((resData) => resData.places),
+          catchError((error) => {
+            console.error(error);
+
+            return throwError(
+              // () => new Error('Something went wrong! Please try again later!')
+              () => new Error(errorMessage)
+            );
+          })
+        )
+    );
+  }
 }
--- http-requests-responses/src/app/places/user-places/user-places.component.ts
+++ http-requests-responses/src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { Place } from '../place.model';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
+import { PlacesService } from '../places.service';
 
 @Component({
   selector: 'app-user-places',
@@ -10,5 +14,34 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+  private placesService = inject(PlacesService);
+  places = this.placesService.loadedUserPlaces;
+  isFetching = signal(false);
+  error = signal('');
+
+  ngOnInit(): void {
+    this.isFetching.set(true);
+    const subscription = this.placesService.loadUserPlaces().subscribe({
+      error: (error: Error) => {
+        this.error.set(error.message);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+
+  // onSelectPlace(selectedPlace: Place) {
+  //   const subscription = this.placesService
+  //     .addPlaceToUserPlaces(selectedPlace.id)
+  //     .subscribe({
+  //       next: (resData) => console.log(resData),
+  //     });
+
+  //   this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  // }
 }
