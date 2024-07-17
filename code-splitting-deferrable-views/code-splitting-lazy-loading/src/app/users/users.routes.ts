5a4835a937a08fb5cea37633eda12049ff8e4208
jp-quintana <juanquintana1996@gmail.com>
2024-07-17 20:45:53 -0300

    wip

---
+++ code-splitting-deferrable-views/code-splitting-lazy-loading/src/app/users/users.routes.ts
@@ -0,0 +1,40 @@
+import { Routes } from '@angular/router';
+
+import { resolveUserTasks, TasksComponent } from '../tasks/tasks.component';
+import {
+  NewTaskComponent,
+  canLeaveEditPage,
+} from '../tasks/new-task/new-task.component';
+import { TasksService } from '../tasks/tasks.service';
+
+export const routes: Routes = [
+  {
+    path: '',
+    providers: [TasksService],
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        component: TasksComponent,
+        // loadComponent: () => {
+        //   return import('../tasks/tasks.component').then(
+        //     (mod) => mod.TasksComponent
+        //   );
+        // },
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage],
+      },
+    ],
+  },
+];
